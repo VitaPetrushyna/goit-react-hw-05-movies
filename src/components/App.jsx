3bb60fb868d236449a2f4546ffa1f88dcaf20244
VitaPetrushyna <masandra2018@ukr.net>
2022-10-20 19:56:02 +0300

    makes a request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
+import { useState, useEffect } from 'react';
+import { getTrendingMovie } from '../services/movies.Api';
+
+export function App() {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function searchMovies() {
+      try {
+        const fetchMovies = await getTrendingMovie(page);
+
+        setMovies(prevState => [fetchMovies]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    searchMovies();
+  }, [page]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            <p>Name: {movie.original_title}</p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
-};
+}
---
+++ src/services/movies.Api.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'c17bf6e924f8c464ad14541e0a1a8fcf';
+
+export const getTrendingMovie = async page => {
+  const response = await axios.get(
+    `${BASE_URL}/trending/movie/day?api_key=${API_KEY}&page=${page}`
+  );
+  return response;
+};
+
+export const getSearchMovie = async (query, page = 1) => {
+  const response = await axios.get(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&page=${page}`
+  );
+  return response.data;
+};
+
+export const getDetailsMovie = async movieId => {
+  const response = await axios.get(
+    `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`
+  );
+  return response.data;
+};
+
+export const getReviewsMovie = async movieId => {
+  const response = await axios.get(
+    `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`
+  );
+  return response.data;
+};
