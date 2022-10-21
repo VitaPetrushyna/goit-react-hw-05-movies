52528b904fde647a4bdfb513b0a673c8c8ae392e
VitaPetrushyna <masandra2018@ukr.net>
2022-10-21 21:53:53 +0300

    not build

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
 import { Home } from '../pages/HomePage/HomePage';
 import { Movies } from '../pages/MoviesPage/MoviesPage';
 import { AppBar } from './AppBar/AppBar';
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react';
-import { getTrendingMovie } from '../../services/movies.Api';
+// import { useState, useEffect } from 'react';
+// import { getTrendingMovie } from '../../services/movies.Api';
 
-export const Home = () => {
-  const [page, setPage] = useState(1);
-  const [query, setQuery] = useState('');
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+// export const Home = () => {
+//   // const [page, setPage] = useState(1);
+//   // const [query, setQuery] = useState('');
+//   const [movies, setMovies] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function searchMovies() {
-      try {
-        const fetchMovies = await getTrendingMovie();
+//   useEffect(() => {
+//     async function searchMovies() {
+//       try {
+//         const fetchMovies = await getTrendingMovie();
 
-        setMovies(prevState => [fetchMovies]);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    searchMovies();
-  }, []);
+//         setMovies(prevState => [fetchMovies]);
+//       } catch (error) {
+//         setError(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//     searchMovies();
+//   }, []);
 
-  return;
-};
+//   return;
+// };
